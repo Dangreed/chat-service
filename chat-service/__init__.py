2dee9f2b425e602801113869c37177b384b277f7
Dangreed <tikondrotas@gmail.com>
2024-10-28 15:24:33 +0200

    function and link initialization

--- chat-service/__init__.py
+++ chat-service/__init__.py
@@ -8,4 +8,24 @@ def create_app():
     cluster = Cluster(['127.0.0.1'], port=9042)
     session = cluster.connect()
 
+    # Register a new channel
+    @app.route('/channels', methods=['PUT'])
+    def put_channel():
+        pass
+
+    # Get channel by ID.
+    @app.route('/channels/<channelId>', methods=['GET'])
+    def get_channel(channelId):
+        pass
+
+    # Delete channel by ID.
+    @app.route('/channels/<channelId>', methods=['DELETE'])
+    def delete_channel(channelId):
+        pass
+
+    # Add message to channel.
+    @app.route('/channels/<channelId>/messages', methods=['PUT'])
+    def put_message():
+        pass
+
     return app
